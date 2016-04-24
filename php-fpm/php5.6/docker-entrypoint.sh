5bde955bed3551a9d1104cdb4bfeb1c41b233bfc
Trurl McByte <trurl@mcbyte.net>
2016-04-24 05:31:45 +0300

    xcache fixes

--- php-fpm/php5.6/docker-entrypoint.sh
+++ php-fpm/php5.6/docker-entrypoint.sh
@@ -16,7 +16,7 @@ write_configs() {
 
 test -e /usr/local/etc/php.configured || write_configs
 test "${OPCACHE_ENABLE}" && echo "zend_extension=opcache.so" > /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
-test "${OPCACHE_DISABLE}" && rm /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
+test "${OPCACHE_DISABLE}" && rm -f /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
 
 cp -f /usr/local/etc/php-fpm.d/zz-docker_global.conf.default /usr/local/etc/php-fpm.d/zz-docker_global.conf
 
@@ -44,10 +44,15 @@ sed -i -e "s/www-data:x:$ORIG_GID:/www-data:x:$WORK_GID:/" /etc/group
 
 chown -R ${WORK_UID}:${WORK_GID} ${ORIG_HOME}
 
-test "${MOD_MEMCACHE}" && echo "${MOD_MEMCACHE}" > /usr/local/etc/php/conf.d/docker-php-ext-memcache.ini
+test "${MOD_MEMCACHE}" && echo "extension=memcache.so" > /usr/local/etc/php/conf.d/docker-php-ext-memcache.ini \
+ && test "${MOD_MEMCACHE}" = "yes" || echo "${MOD_MEMCACHE}" >> /usr/local/etc/php/conf.d/docker-php-ext-memcache.ini
 
 test "${MOD_XDEBUG}" && cat /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini.saved > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
  && test "${MOD_XDEBUG}" = "yes" || echo "${MOD_XDEBUG}" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
+test "${MOD_XCACHE}" && cat /usr/local/etc/php/conf.d/docker-php-ext-xcache.ini.saved > /usr/local/etc/php/conf.d/docker-php-ext-xcache.ini \
+ && test "${MOD_XCACHE}" = "yes" || echo "${MOD_XCACHE}" >> /usr/local/etc/php/conf.d/docker-php-ext-xcache.ini
+
 
 if [ $# -eq 0 ]; then
     /usr/local/sbin/php-fpm
